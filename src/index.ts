54bb799cd48810aa09f356a997913849f2b3c3c4
Chiang Fong Lee <myself@cflee.net>
2025-04-20 23:21:16 +0800

    add rudimentary webhook handler

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,27 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response("Hello World!");
+		if (request.method === "POST") {
+			const url = new URL(request.url);
+			if (url.pathname !== "/telegramWebhook") {
+				console.log(`Received unexpected path name: ${url.pathname}`);
+				return new Response("Not Found", { status: 404 });
+			}
+			const webhookSecret = env.TELEGRAM_BOT_WEBHOOK_SECRET;
+			const receivedToken = request.headers.get(
+				"X-Telegram-Bot-Api-Secret-Token",
+			);
+			if (receivedToken !== webhookSecret) {
+				console.log(`Received unexpected token: ${receivedToken}}`);
+				return new Response("Unauthorized", { status: 401 });
+			}
+			const update = await request.json();
+			console.log(update);
+			return new Response("OK", { status: 200 });
+		}
+		console.log(
+			`Received unexpected method: ${request.method} to ${request.url}`,
+		);
+		return new Response("Not Found", { status: 404 });
 	},
 } satisfies ExportedHandler<Env>;
